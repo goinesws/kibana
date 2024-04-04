3f3a32550c3ed84f9fa9e926d3ea81c25e873ff6
rafaellerichel <rafaellerichel@gmail.com>
2024-04-04 12:21:18 +0700

    routes edit

--- api/routes/transactionRouter.js
+++ api/routes/transactionRouter.js
@@ -18,33 +18,79 @@ const { authorize, listFiles, uploadFile } = require("../models/googleModel");
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
+
+
+
+//inquiry invoice transaksi client
 router.get(
 	"/invoice/:transactionId",
 	transactionController.getTransactionInvoice
 );
+
+//inquiry invoice transaksi freelancer
 router.get(
 	"/invoice/:transactionId/completed",
 	transactionController.getTransactionInvoice
 );
+
+//inquiry detail pesanan tugas client
 router.get(
 	"/task/client/:transactionId",
 	transactionController.getTransactionDetailsClientTask
 );
+
+//inquiry detail pesanan tugas freelancer
 router.get(
 	"/task/freelancer/:transactionId",
 	transactionController.getTransactionDetailsFreelancerTask
 );
+
+//inquiry activity pesanan client
 router.get(
 	"/:transactionId/client/activity",
 	transactionController.getClientTransactionActivity
 );
+
+//Inquiry Detail Pesanan Layanan Client
 router.get(
 	"/service/client/:transactionId",
 	transactionController.getTransactionDetailsClientService
 );
+
+//inquiry detail pesanan layanan freelancer
 router.get(
 	"/service/freelancer/:transactionId",
 	transactionController.getTransactionDetailsFreelancerService
 );
+
+//inquiry activity pesanan freelancer
+
+//send requirement
+
+//send message
+
+//send additional file
+
+//send result
+
+//ask return
+
+//cancel return
+
+//ask revision
+
+//complete transaksi
+
+//manage cancellation
+
+//call admin
+
+//ask cancellation
+
+//cancel cancellation
+
+//manage return
+
+//send feedback - transaction
 
 module.exports = router;
